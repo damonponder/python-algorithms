3cf699d27ac57f32471cd96466507c3202d4c60c
Damon Ponder <dr4602@gmail.com>
2026-04-14 19:17:00 -0500

    finding duplicat numbers, returning duplicates,and finding first duplicate

--- duplicate.py
+++ duplicate.py
@@ -1,10 +1,20 @@
 nums = [1, 2, 3, 4, 5, 2, 3]
 
-def find_duplicate(nums):
+def find_duplicates(nums):
     seen = set()
+    duplicates = set()
 
+    for num in nums:
+        if num in seen:
+            duplicates.add(num)
+        seen.add(num)
+    return list(duplicates)
+
+def first_duplicate(nums):
+    seen = set()
     for num in nums:
         if num in seen:
             return num
         seen.add(num)
-    return None
+
+
